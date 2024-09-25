7488b6e97e76e88df678ee8f92a0216e2ad39fd7
masindes <maasinde.sylvester@gmail.com>
2024-09-25 14:13:36 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength) {
+    if (rideLength <= 400) {
+      return 'This one is on me!';
+    } else if (rideLength <=2000) {
+      return 'That will be twenty bucks.';
+    } else if (rideLength <=2500)
+      return 'I will gladly take your thirty bucks.';
+      else {
+      return 'No can do.';
+    } 
+  }
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
   // Write your code here!
 }
